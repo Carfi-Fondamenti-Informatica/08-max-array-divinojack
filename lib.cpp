fde4ba7f7284fd4df890023fe14449b01f7df98f
divinojack <115471906+divinojack@users.noreply.github.com>
2022-12-12 15:23:39 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
+
+float NumeroMassimo(const float array[],int d) {
+    float max;
+    int i;
+    max = array[0];
+    for (i = 1; i < d; i++) {
+
+        if (array[i] > max)
+            max = array[i];
+    }
+    return max;
+}
 
