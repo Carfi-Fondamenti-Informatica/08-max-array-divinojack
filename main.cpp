5c548e4f7d73078250632cd0e985568aca539b5f
divinojack <115471906+divinojack@users.noreply.github.com>
2022-12-12 15:22:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int a;
+    cin >> a;
+    float array[a];
+    for (int i = 0; i < a; i++) {
+        cin >> array[i];
+    }
+
+    cout << NumeroMassimo(array, a);
+    return 0;
+
 }
