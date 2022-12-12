dcae85eea714dd1b9dcaa8252c942a487569ab63
divinojack <115471906+divinojack@users.noreply.github.com>
2022-12-12 15:30:03 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,4 +1,4 @@
 #ifndef ESERCITAZIONE_8_MIA_LIB_H
 #define ESERCITAZIONE_8_MIA_LIB_H
-float NumeroMassimo(const float array[],int d);
+float maggiore(float numero[], int n);
 #endif //ESERCITAZIONE_8_MIA_LIB_H
